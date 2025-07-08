b2fe901d6cb7d228434863ac4c00bf903d71fdba
Rahul-Office-1818 <rahul.office.mail1818@gmail.com>
2025-07-08 17:53:02 +0530

    Change the button of apply filter and remove filter

--- assets/js/history.js
+++ assets/js/history.js
@@ -58,6 +58,59 @@ async function onLogsLoad() {
   Toast.fire({ icon: "warning", title: response.message });
 }
 
+
+
+async function onLogsLoadWithoutDateFilter() {
+  document.querySelector("#startdate").value = "";
+  document.querySelector("#enddate").value = "";
+  let selectedBlock = document.querySelector("#blockOptions").value;
+
+  const get = await fetch(
+    "/api/logs?blockId=" + selectedBlock,
+    {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    }
+  );
+
+  if (get.status === 200) {
+    let { payload } = await get.json();
+    logsTableBodySelector.innerHTML = "";
+    payload.forEach((el, idx) => {
+      let d = new Date(el.dateTime);
+      let seconds = d.getSeconds().toString().padStart(2, "0");
+      let minutes = d.getMinutes().toString().padStart(2, "0");
+      let hours = d.getHours().toString().padStart(2, "0");
+      let months = (d.getMonth() + 1).toString().padStart(2, "0");
+      let date = d.getDate().toString().padStart(2, "0");
+      let datetime = `${date}/${months}/${d.getFullYear()} ${hours}:${minutes}:${seconds}`;
+      let seconds2 = d.getSeconds().toString().padStart(2, "0");
+      let StatTime = `${d.getDate()}/${d.getMonth() + 1}/${d.getFullYear()} ${hours}:${minutes}:${seconds2}`;
+      
+      const tr = document.createElement("tr");
+      tr.classList.add("text-center", "text-xl", "divide-gray-200");
+      tr.setAttribute("status", el.status);
+      tr.setAttribute("blockId", el.blockId);
+      tr.innerHTML = `<td scope="col" class="py-2">${idx + 1}</td>
+            <td scope="col" class="py-2">${el.jammerName}</td>
+            <td scope="col" class="py-2">Block ${el.blockId}</td>
+            <td scope="col" class="py-2" datetime="${StatTime}">${el.jammer_on}</td>
+            <td scope="col" class="py-2">${el.jammer_off}</td>
+            <td scope="col" class="py-2">${el.diffrence}</td>`;
+      logsTableBodySelector.appendChild(tr);
+    });
+
+    return;
+  }
+
+  Toast.fire({ icon: "warning", title: "Failed to fetch logs" });
+}
+
+
+
+
 const filter_log = document.querySelector("#date-filter");
 const filter_remove_log = document.querySelector("#date-filter-remove");
 
@@ -143,7 +196,7 @@ initial();
 
 document.querySelector("#date-filter").addEventListener("click", onLogsLoad);
 document.querySelector("#logs-reload").addEventListener("click", onLogsReload);
-document.querySelector("#date-filter-remove").addEventListener("click", onLogsLoad);
+document.querySelector("#date-filter-remove").addEventListener("click", onLogsLoadWithoutDateFilter);
 // document.querySelector("#powerfailure-logs").addEventListener("click", pflogs);
 
 document
